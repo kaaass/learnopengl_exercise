e2a3f3929e22482f353e39a6cb719d7e773e355f
KAAAsS <admin@kaaass.net>
2020-02-16 14:31:43 +0800

    deep testing

---
+++ src/04_advanced_opengl/depth_testing.cpp
@@ -0,0 +1,247 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "../shader_s.h"
+#include "../model.h"
+#include "../camera.h"
+
+#include "block_and_plane_vertices.h"
+
+const char* vertShaderPath = "shader/depth_testing.vs";
+const char* fragShaderPath = "shader/depth_testing.fs";
+
+int screenWidth = 1280;
+int screenHeight = 720;
+
+Shader* shader = nullptr;
+
+unsigned int cubeVAO, cubeVBO;
+unsigned int planeVAO, planeVBO;
+unsigned int cubeTexture;
+unsigned int floorTexture;
+Model* model = nullptr;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+Camera* camera = nullptr;
+float lastX = screenWidth / 2;
+float lastY = screenHeight / 2;
+bool firstMouse = true;
+
+void prepareDraw() {
+    // Create shader
+    shader = new Shader(vertShaderPath, fragShaderPath);
+    // Create camera
+    camera = new Camera(glm::vec3(1.0f, 1.0f, 5.0f));
+
+    // cube VAO
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &cubeVBO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glBindVertexArray(0);
+    // plane VAO
+    glGenVertexArrays(1, &planeVAO);
+    glGenBuffers(1, &planeVBO);
+    glBindVertexArray(planeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glBindVertexArray(0);
+
+    // load textures
+    cubeTexture  = loadTexture("image/marble.jpg");
+    floorTexture = loadTexture("image/metal.png");
+
+    // shader
+    shader->use();
+    shader->setInt("texture1", 0);
+
+    glDepthFunc(GL_LESS);
+}
+
+void drawStaff() {
+    shader->use();
+
+    // Projection matrix
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(camera->zoom), (float) screenWidth / screenHeight, 0.1f, 100.0f);
+
+    // Pass the transform matrixs
+    shader->setMat4("view", camera->getViewMatrix());
+    shader->setMat4("projection", projection);
+
+    glm::mat4 model = glm::mat4(1.0f);
+
+    // cubes
+    glBindVertexArray(cubeVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, cubeTexture);
+
+    model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+    shader->setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+    shader->setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    // floor
+    glBindVertexArray(planeVAO);
+    glBindTexture(GL_TEXTURE_2D, floorTexture);
+
+    shader->setMat4("model", glm::mat4(1.0f));
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+void processInput(GLFWwindow *window);
+
+int main() {
+    glfwInit();
+    // OpenGL Version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Using core profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // For Mac OS X:
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Create window object
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // Using GLAD to load OpenGL functions
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    // Define the Viewport
+    glViewport(0, 0, screenWidth, screenHeight);
+    // Register callbacks
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    // Capture the mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // Configuration
+    glEnable(GL_DEPTH_TEST);
+
+    // Prepare for drawing
+    prepareDraw();
+    
+    // Start render loop
+    while (!glfwWindowShouldClose(window)) {
+        // Input
+        processInput(window);
+
+        // Clear Screen
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Draw
+        drawStaff();
+
+        // Frame calc
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // Swap double buffer
+        glfwSwapBuffers(window);
+        // Deal with the events
+        glfwPollEvents();    
+    }
+
+    delete shader;
+    glfwTerminate();
+    return 0;
+}
+
+/*
+ * Callbacks
+ */
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+    screenWidth = width;
+    screenHeight = height;
+}
+
+bool mouseCap = true;
+double lstChangeMouse = 0;
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+    // Camera Pos
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera->processKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera->processKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera->processKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera->processKeyboard(RIGHT, deltaTime);
+    // Release mouse
+    if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS) {
+        double now = glfwGetTime();
+        if (now - lstChangeMouse > 0.2) {
+            lstChangeMouse = now;
+            mouseCap = !mouseCap;
+            if (mouseCap) {
+                glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+            } else {
+                glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+            }
+        } 
+    }
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    if (mouseCap) {
+        camera->processMouseMovement(xoffset, yoffset);
+    }
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    camera->processMouseScroll(yoffset);
+}
--- src/model.h
+++ src/model.h
@@ -17,6 +17,10 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
+unsigned int textureFromFile(string filepath, string directory, int mode = GL_RGBA);
+
+unsigned int loadTexture(string filepath, int mode = GL_RGBA);
+
 class Model {
 public:
 
@@ -141,46 +145,50 @@ private:
         }
         return textures;
     }
-
-    int textureFromFile(string filepath, string directory, int mode = GL_RGBA) {
-        unsigned int texture = 0;
-        int pos = 0;
-        // Win路径修正
-        while ((pos = filepath.find("\\")) >= 0) {
-            filepath.replace(pos, 1, "/");
-        }
-        std::cout << "Start loading model texture: " << directory << " , " << filepath << std::endl;
-        filepath = directory + "/" + filepath;
-        // Create texture
-        glGenTextures(1, &texture);
-        // Load and generate texture
-        int width, height, nrChannels;
-        // stbi_set_flip_vertically_on_load(true);
-        unsigned char *data = stbi_load(filepath.c_str(), &width, &height, &nrChannels, 0);
-        if (data) {
-            if (nrChannels == 1)
-                mode = GL_RED;
-            else if (nrChannels == 3)
-                mode = GL_RGB;
-            else if (nrChannels == 4)
-                mode = GL_RGBA;
-            // std::cout << nrChannels << std::endl;
-            glBindTexture(GL_TEXTURE_2D, texture);
-            // Load data
-            glTexImage2D(GL_TEXTURE_2D, 0, mode, width, height, 0, mode, GL_UNSIGNED_BYTE, data);
-            glGenerateMipmap(GL_TEXTURE_2D);
-            // Configure wrap and filter type
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        } else {
-            std::cout << "Failed to load model texture" << std::endl;
-        }
-        // Free image
-        stbi_image_free(data);
-        return texture;
-    }
 };
+
+unsigned int textureFromFile(string filepath, string directory, int mode) {
+    int pos = 0;
+    // Win路径修正
+    while ((pos = filepath.find("\\")) >= 0) {
+        filepath.replace(pos, 1, "/");
+    }
+    filepath = directory + "/" + filepath;
+    return loadTexture(filepath, mode);
+}
+
+unsigned int loadTexture(string filepath, int mode) {
+    unsigned int texture = 0;
+    std::cout << "Start loading texture: " << filepath << std::endl;
+    // Create texture
+    glGenTextures(1, &texture);
+    // Load and generate texture
+    int width, height, nrChannels;
+    // stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load(filepath.c_str(), &width, &height, &nrChannels, 0);
+    if (data) {
+        if (nrChannels == 1)
+            mode = GL_RED;
+        else if (nrChannels == 3)
+            mode = GL_RGB;
+        else if (nrChannels == 4)
+            mode = GL_RGBA;
+        // std::cout << nrChannels << std::endl;
+        glBindTexture(GL_TEXTURE_2D, texture);
+        // Load data
+        glTexImage2D(GL_TEXTURE_2D, 0, mode, width, height, 0, mode, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        // Configure wrap and filter type
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    // Free image
+    stbi_image_free(data);
+    return texture;
+}
 
 #endif
